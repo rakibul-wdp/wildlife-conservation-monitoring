de346d384c3cb7d1858351db2cc4d79dbb9db742
rakibul-wdp <rakibul.wdp@gmail.com>
2025-05-25 20:26:26 +0600

    solved problem 1

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -43,3 +43,6 @@ INSERT INTO sightings (ranger_id, species_id, sighting_time, location, notes) VA
 (3, 3, '2024-05-15 09:10:00', 'Bamboo Grove East', 'Feeding observed'),
 (2, 1, '2024-05-18 18:30:00', 'Snowfall Pass', NULL);
 
+-- Problem 1
+INSERT INTO rangers (name, region) VALUES ('Derek Fox', 'Coastal Plains');
+
