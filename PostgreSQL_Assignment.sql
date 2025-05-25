72116ff7a9f13f6422d9e5499f1e530c2f90cfd0
rakibul-wdp <rakibul.wdp@gmail.com>
2025-05-25 21:21:23 +0600

    add problem 3 solution

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -48,3 +48,6 @@ INSERT INTO rangers (name, region) VALUES ('Derek Fox', 'Coastal Plains');
 
 -- Problem 2
 SELECT COUNT(DISTINCT species_id) AS unique_species_count FROM sightings;
+
+-- Problem 3
+SELECT * FROM sightings WHERE location LIKE '%Pass%';
